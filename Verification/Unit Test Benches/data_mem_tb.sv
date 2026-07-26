c62a16d6b4230fe690c97a845798603eb0698d17
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-26 17:12:23 -0400

    Update data_mem_tb.sv

--- Verification/Unit Test Benches/data_mem_tb.sv
+++ Verification/Unit Test Benches/data_mem_tb.sv
@@ -10,3 +10,17 @@ module dmem_tb;
   logic [XLEN-1:0] tb_read_data;
 
   int unsigned total_tests = 0;
+  int unsigned passed_tests = 0;
+
+  dmem #(.ram_size(1024)) duv (
+    .clk(clk),
+    .byte_addr(tb_byte_addr),
+    .wdata(tb_wdata),
+    .mem_read(mem_read),
+    .mem_write(mem_write),
+    .mem_size(mem_size),
+    .mem_unsigned(mem_unsigned),
+    .read_data(tb_read_data)
+  );
+
+  always #5 clk = ~clk;

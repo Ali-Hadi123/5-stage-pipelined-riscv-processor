06dbbc19138e4bf1ce38cc3e62aed66d3bface7a
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-26 20:05:26 -0400

    Update data_mem_tb.sv

--- Verification/Unit Test Benches/data_mem_tb.sv
+++ Verification/Unit Test Benches/data_mem_tb.sv
@@ -24,3 +24,22 @@ module dmem_tb;
   );
 
   always #5 clk = ~clk;
+
+  task write_data(
+    input logic [XLEN-1:0] addr,
+    input logic [XLEN-1:0] data,
+    input logic size, is_unsigned
+  );
+
+    @(negedge clk);
+    mem_read = 1'b0;
+    mem_write = 1'b1;
+
+    tb_byte_addr = addr;
+    tb_wdata = data;
+    mem_size = size;
+    mem_unsigned = is_unsigned;
+
+    @(negedge clk);
+    mem_write = 1'b0;
+  endtask

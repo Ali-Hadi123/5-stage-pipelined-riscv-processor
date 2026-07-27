54ee13b5950c52d4270c945a4fad6c0505ac4a03
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-26 22:09:28 -0400

    Update data_mem_tb.sv

--- Verification/Unit Test Benches/data_mem_tb.sv
+++ Verification/Unit Test Benches/data_mem_tb.sv
@@ -52,4 +52,42 @@ module dmem_tb;
     input logic is_unsigned
   );
 
-    
+    @(negedge clk);
+    mem_read = 1'b1;
+    mem_write = 1'b0;
+
+    tb_byte_addr = addr;
+    mem_size = size;
+    mem_unsigned = is_unsigned;
+
+    @(negedge clk);
+    mem_read = 1'b0;
+  endtask
+
+  initial begin
+    $dumpfile("data_mem_tb.vcd");
+    $dumpvars(0, dmem_tb);
+
+    clk = 0;
+    tb_byte_addr = 0;
+    tb_wdata = 0;
+    mem_read = 0;
+    mem_write = 0;
+    mem_size = MEM_BYTE;
+    mem_unsigned = 0;
+    tb_read_data = 0;
+
+    $display("STARTING DATA MEMORY TESTING:");
+
+    write_data(32'd0, 32'd12345, MEM_WORD, 1'b1);  //Testing writing a word to dmem at address 0x0.
+    read_data(32'd0, MEM_WORD, 1'b1);
+    total_tests++;
+
+    #10;
+
+    assert(tb_read_data === 32'd12345) begin
+      passed_tests++;
+      $display("Test 1 Passed.");
+    end
+    else
+      $error("TEST 1 Failed:\nExpected: 32'd12345\n Got: %0d", tb_read_data);

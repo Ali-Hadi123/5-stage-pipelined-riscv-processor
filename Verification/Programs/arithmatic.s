785a52e5b9808e037c51ac56eba28da0836c8efe
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-28 15:47:26 -0400

    Update and rename arithmatic.s to arithmatic_test.s

--- Verification/Programs/arithmatic.s
+++
@@ -1,3 +0,0 @@
-addi x1, x0, 1 ; x1 = 1
-addi x2, x0, 1 ; x2 = 1
-add x3, x1, x2 ; x3 = x1 + x2
---
+++ Verification/Programs/arithmatic_test.s
@@ -0,0 +1,6 @@
+addi x1, x0, 1 ; x1 = 1
+addi x2, x0, 1 ; x2 = 1
+add x3, x1, x2 ; x3 = x1 + x2 = 2
+subi x4, x3, 7 ; x4 = x3 - 7 = -5
+sub x5, x1, x4 ; x5 = x1 - x4 = 6
+and x6, x1, x0 ; x6 = x1 & x0 = 0

b0fc89974fee38cd0d0caefd15215f6ecd38c57e
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-30 00:46:05 -0400

    Update top_level_tb.sv

--- Verification/Top Level Tests/top_level_tb.sv
+++ Verification/Top Level Tests/top_level_tb.sv
@@ -59,3 +59,28 @@ module top_tb;
     verify_register(32'd7, 32'd7, "Test 7");
     verify_register(32'd8, 32'd6, "Test 8");
     verify_register(32'd9, -32'd10, "Test 9");
+    verify_register(32'd10, 32'd1, "Test 10");
+    verify_register(32'd11, 32'd0, "Test 11");
+    verify_register(32'd12, 32'd0, "Test 12");
+    verify_register(32'd13, 32'd1, "Test 13");
+    verify_register(32'd14, 32'd1, "Test 14");
+    verify_register(32'd15, 32'd16, "Test 15");
+    verify_register(32'd16, 32'd32, "Test 16");
+    verify_register(32'd17, 32'd8, "Test 17");
+    verify_register(32'd18, -32'd8, "Test 18");
+    verify_register(32'd19, -32'd4, "Test 19");
+    verify_register(32'd20, 32'd8, "Test 20");
+    verify_register(32'd21, 32'd4, "Test 21");
+    verify_register(32'd22, -32'd1, "Test 22");
+    verify_register(32'd23, 32'd1, "Test 23");
+    verify_register(32'd24, 32'd7, "Test 24");
+    verify_register(32'd25, 32'd0, "Test 25");
+    verify_register(32'd26, 32'd1, "Test 26");
+    verify_register(32'd27, 32'd0, "Test 27");
+
+    $display("\nARITHMATIC TESTING COMPLETED.\n");
+
+    $display("TESTING COMPLETED\nResults: %0d/%0d tests passed.", passed_tests, total_tests);
+    $finish
+  end
+endmodule

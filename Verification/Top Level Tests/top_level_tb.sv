a7c973d833d0a7ccb9dc448acd7fcefa59e4fc84
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-29 22:30:22 -0400

    Update top_level_tb.sv

--- Verification/Top Level Tests/top_level_tb.sv
+++ Verification/Top Level Tests/top_level_tb.sv
@@ -1,3 +1,18 @@
 import riscv_pkg::*;
+`timescale 1ns/1ps
 
 module top_tb;
+
+  logic clk;
+  logic rst;
+  logic illegal_instr;
+
+  top #(
+    .ram_size(1024),
+    .rom_size(1024),
+    .init_mem("arithmatic_test.hex")
+  ) duv_arithmatic (
+    .clk(clk),
+    .rst(rst),
+    .illegal_instr(illegal_instr)
+  );

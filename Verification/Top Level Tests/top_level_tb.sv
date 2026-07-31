5e4189cdd1df8192f3e8e5cfe0b5bff33fb1da19
Ali-Hadi123 <77ali.h77@gmail.com>
2026-07-31 15:21:30 -0400

    Update top_level_tb.sv

--- Verification/Top Level Tests/top_level_tb.sv
+++ Verification/Top Level Tests/top_level_tb.sv
@@ -13,6 +13,56 @@ module top_tb;
 
   always #5 clk = ~clk;
 
+  top #(
+    .ram_size(1024),
+    .rom_size(27),
+    .init_mem("arithmatic_test.hex")
+  ) duv_arithmatic (
+    .clk(clk),
+    .rst(rst),
+    .illegal_instr(illegal_instr)
+  );
+
+  top #(
+    .ram_size(1024),
+    .rom_size(26),
+    .init_mem("branch_test.hex")
+  ) duv_branch (
+    .clk(clk),
+    .rst(rst),
+    .illegal_instr(illegal_instr)
+  );
+
+  top #(
+    .ram_size(1024),
+    .rom_size(7),
+    .init_mem("jump_test.hex")
+  ) duv_jump (
+    .clk(clk),
+    .rst(rst),
+    .illegal_instr(illegal_instr)
+  );
+
+  top #(
+    .ram_size(1024),
+    .rom_size(34),
+    .init_mem("load_store_test.hex")
+  ) duv_load_store (
+    .clk(clk),
+    .rst(rst),
+    .illegal_instr(illegal_instr)
+  );
+
+  top #(
+    .ram_size(1024),
+    .rom_size(5),
+    .init_mem("upper_immediate_test.hex")
+  ) duv_upper_immediate (
+    .clk(clk),
+    .rst(rst),
+    .illegal_instr(illegal_instr)
+  );
+
   task verify_register(
     input logic [XLEN-1:0] regs [0:REG_COUNT-1],
     input logic [REG__ADDR_W-1:0] rn,
@@ -31,20 +81,25 @@ module top_tb;
     end
   endtask
 
-  top #(
-    .ram_size(1024),
-    .rom_size(27),
-    .init_mem("arithmatic_test.hex")
-  ) duv_arithmatic (
-    .clk(clk),
-    .rst(rst),
-    .illegal_instr(illegal_instr)
+  task verify_memory(
+    input logic [XLEN-1:0] regs [0:REG_COUNT-1],
+    input logic [REG__ADDR_W-1:0] rn,
+    input logic [XLEN-1:0] exp_rdata,
+    input string test_name
   );
+
+    @(posedge clk) begin
+      total_tests++;
+      assert (exp_rdata === regs[rn]) begin
+        passed_tests++;
+        $display("Passed %s!", test_name);
+      end
+      else
+        $error("Failed %s!\nExpected: %0d\nGot: %0d", test_name, exp_rdata, regs[rn]);
+    end
+  endtask
   
   initial begin
-    $dumpfile("top_tb.vcd");
-    $dumpvars(0, top_tb);
-
     clk = 0;
     rst = 1;
     #10;
@@ -54,33 +109,33 @@ module top_tb;
 
     $display("STARTING ARITHMATIC TESTING:\n");
 
-    verify_register(duv_arithmatic.u_regf.regs, 32'd1, 32'd5, "Test 1");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd2, 32'd3, "Test 2");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd3, 32'd8, "Test 3");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd4, 32'd2, "Test 4");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd5, -32'd1, "Test 5");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd6, 32'd5, "Test 6");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd7, 32'd7, "Test 7");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd8, 32'd6, "Test 8");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd9, -32'd10, "Test 9");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd10, 32'd1, "Test 10");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd11, 32'd0, "Test 11");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd12, 32'd0, "Test 12");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd13, 32'd1, "Test 13");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd14, 32'd1, "Test 14");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd15, 32'd16, "Test 15");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd16, 32'd32, "Test 16");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd17, 32'd8, "Test 17");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd18, -32'd8, "Test 18");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd19, -32'd4, "Test 19");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd20, 32'd8, "Test 20");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd21, 32'd4, "Test 21");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd22, -32'd1, "Test 22");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd23, 32'd1, "Test 23");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd24, 32'd7, "Test 24");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd25, 32'd0, "Test 25");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd26, 32'd1, "Test 26");
-    verify_register(duv_arithmatic.u_regf.regs, 32'd27, 32'd0, "Test 27");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd1, 32'd5, "ARTH Test 1");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd2, 32'd3, "ARTH Test 2");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd3, 32'd8, "ARTH Test 3");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd4, 32'd2, "ARTH Test 4");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd5, -32'd1, "ARTH Test 5");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd6, 32'd5, "ARTH Test 6");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd7, 32'd7, "ARTH Test 7");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd8, 32'd6, "ARTH Test 8");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd9, -32'd10, "ARTH Test 9");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd10, 32'd1, "ARTH Test 10");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd11, 32'd0, "ARTH Test 11");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd12, 32'd0, "ARTH Test 12");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd13, 32'd1, "ARTH Test 13");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd14, 32'd1, "ARTH Test 14");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd15, 32'd16, "ARTH Test 15");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd16, 32'd32, "ARTH Test 16");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd17, 32'd8, "ARTH Test 17");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd18, -32'd8, "ARTH Test 18");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd19, -32'd4, "ARTH Test 19");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd20, 32'd8, "ARTH Test 20");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd21, 32'd4, "ARTH Test 21");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd22, -32'd1, "ARTH Test 22");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd23, 32'd1, "ARTH Test 23");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd24, 32'd7, "ARTH Test 24");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd25, 32'd0, "ARTH Test 25");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd26, 32'd1, "ARTH Test 26");
+    verify_register(duv_arithmatic.u_regf.regs, 32'd27, 32'd0, "ARTH Test 27");
 
     $display("\nTESTING COMPLETED\nResults: %0d/%0d tests passed.", passed_tests, total_tests);
     $display("\nARITHMATIC TESTING COMPLETED.\n");

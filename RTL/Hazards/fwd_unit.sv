acbdfb27f575d1b213f3e465c9f96703b4cb4862
Ali Hadi <77ali.h77@gmail.com>
2026-08-15 19:06:31 -0400

    added hzrd unit and fixed typo in fwding

--- RTL/Hazards/fwd_unit.sv
+++ RTL/Hazards/fwd_unit.sv
@@ -1,9 +1,9 @@
 import riscv_pkg::*;
 
 module fwd_unit(
-    input logic [REG_ADDR_WIDTH-1:0] rs1E, rs2E,
-    input logic [REG_ADDR_WIDTH-1:0] rdM,
-    input logic reg_writeM,
+    input logic [REG_ADDR_W-1:0] rs1E, rs2E,
+    input logic [REG_ADDR_W-1:0] rdM, rdW,
+    input logic reg_writeM, reg_writeW,
     output fwdA_e fwdA,
     output fwdB_e fwdB
 );
@@ -12,4 +12,19 @@ module fwd_unit(
         fwdA_e = FWD_NONE_A;
         fwdB_e = FWD_NONE_B;
 
-        
+        if (((rs1E == rdM) & reg_writeM) & (rs1E != 0))
+            fwdA_e = FWD_MEM_A;
+        else if (((rs1E == rdW) & reg_writeW) & (rs1E != 0))
+            fwdA_e = FWD_WB_A;
+        else
+            fwdA_e = FWD_NONE_A;
+
+        if (((rs2E == rdM) & reg_writeM) & (rs2E != 0))
+            fwdB_e = FWD_MEM_B;
+        else if (((rs2E == rdW) & reg_writeW) & (rs2E != 0))
+            fwdB_e = FWD_WB_B;
+        else
+            fwdB_e = FWD_NONE_B;
+    end
+
+endmodule
---
+++ RTL/Hazards/hzrd_unit.sv
@@ -0,0 +1,26 @@
+import riscv_pkg::*;
+
+module hzrd_unit(
+    input logic mem_readE,
+    input logic [REG_ADDR_W-1:0] rs1D, rs2D,
+    input logic [REG_ADDR_W-1:0] rdE,
+    input pc_src_e pc_srcE,
+    output logic stallF, stallD,
+    output logic flushD, flushE 
+);
+
+    logic lw_stall;
+    logic branch_mispredict;
+
+    always_comb begin
+        lw_stall = ((mem_readE & ((rdE == rs1D) | (rdE == rs2D))) & (rdE != 0));
+        branch_mispredict = (pc_srcE != PC_PLUS4);
+
+        stallF = lw_stall;
+        stallD = lw_stall;
+
+        flushD = branch_mispredict;
+        flushE = lw_stall | branch_mispredict;
+    end
+
+endmodule

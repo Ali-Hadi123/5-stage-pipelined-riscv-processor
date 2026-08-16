11b57ec5c63ce24865980df15a96116d75229dda
Ali Hadi <77ali.h77@gmail.com>
2026-08-16 15:28:12 -0400

    added stall to pc

--- RTL/Data Path/pc.sv
+++ RTL/Data Path/pc.sv
@@ -3,6 +3,7 @@ import riscv_pkg::*;
 module pc (
     input clk,
     input rst,
+    input stall,
     input logic [XLEN-1:0] next_pc,
     output logic [XLEN-1:0] pc_out
 );
@@ -10,7 +11,7 @@ module pc (
 always_ff @(posedge clk or posedge rst)
     if (rst)
         pc_out <= 0;    //Forces the program counter to 0 in the event of a reset signal.
-    else
+    else if (~stall)
         pc_out <= next_pc; //Sets the program counter to next_pc in response to the clock's rising edge.
         
 endmodule

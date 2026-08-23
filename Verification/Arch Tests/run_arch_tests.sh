525a21835a6887b326316196841623924988bf6e
Ali Hadi <77ali.h77@gmail.com>
2026-08-23 19:51:05 -0400

    .

--- Verification/Arch Tests/run_arch_tests.sh
+++ Verification/Arch Tests/run_arch_tests.sh
@@ -21,7 +21,7 @@ CC="${PREFIX}gcc"
 OBJCOPY="${PREFIX}objcopy"
 NM="${PREFIX}nm"
 
-TEST_SRC_DIR="$ARCH_TEST_DIR/riscv-arch-test/tests/rv32i/I"
+TEST_SRC_DIR="$ARCH_TEST_DIR/riscv-test-suite/rv32i_m/I/src"
 ENV_DIR="$ARCH_TEST_DIR/riscv-test-suite/env"
 
 WORKDIR="$(mktemp -d)"

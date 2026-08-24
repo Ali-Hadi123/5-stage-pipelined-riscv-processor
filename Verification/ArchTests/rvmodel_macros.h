f19724ebfc3cf9335e85f62b0371f3aeb676d3c6
Ali Hadi <77ali.h77@gmail.com>
2026-08-24 18:37:46 -0400

    uploaded rvmodel macros file

--- Verification/ArchTests/rvmodel_macros.h
+++ Verification/ArchTests/rvmodel_macros.h
@@ -17,7 +17,8 @@
 # Perform boot operations. Can be empty or left undefined unless needed for
 # DUT-specific behavior such as turning on a memory controller or
 # initializing custom state.
-//#define RVMODEL_BOOT
+
+#define RVMODEL_BOOT
 
 // Custom RVMODEL_BOOT_TO_MMODE overrides default RVTEST_BOOT_TO_MMODE
 // if defined.  For most DUTs, the default should work and this macro
@@ -25,7 +26,8 @@
 // macro as blank to bypass the boot process.  If a nonconforming
 // M-mode is implemented, define this macro to set up the necessary
 // state in a fashion similar to RVTEST_BOOT_TO_MMODE.
-#define RVMODEL_BOOT_TO_MMODE
+
+//#define RVMODEL_BOOT_TO_MMODE
 
 ##### TERMINATION #####
 

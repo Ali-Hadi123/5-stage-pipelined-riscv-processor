57850301e009e6788dd84e3b2d11ff16142ed837
Ali Hadi <77ali.h77@gmail.com>
2026-09-02 16:54:13 -0400

    decreased fd reg fanout

--- RTL/Pipeline/fd_reg.sv
+++ RTL/Pipeline/fd_reg.sv
@@ -9,15 +9,9 @@ module fd_reg(
     output fd_reg_t d_out
 );
 
-  function automatic fd_reg_t bubble();
-    bubble.instr    = 32'h0000_0013;     //NOP machine code
-    bubble.pc       = '0;
-    bubble.pc_plus4 = '0;
-  endfunction
-
   always_ff @(posedge clk or posedge rst) begin
     if (rst | flush)
-        d_out <= bubble();
+        d_out.instr <= 32'h0000_0013;
     else if (~stall)
         d_out <= d_in;
   end

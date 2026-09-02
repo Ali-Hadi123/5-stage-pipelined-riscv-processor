ba19b9b0de9d889970d1069cf78d5e933f17016f
Ali Hadi <77ali.h77@gmail.com>
2026-09-02 17:56:16 -0400

    optimized flush

--- RTL/Pipeline/fd_reg.sv
+++ RTL/Pipeline/fd_reg.sv
@@ -10,10 +10,22 @@ module fd_reg(
 );
 
   always_ff @(posedge clk or posedge rst) begin
-    if (rst | flush)
-        d_out.instr <= 32'h0000_0013;
-    else if (~stall)
-        d_out <= d_in;
-  end
+    if (rst) begin
+      d_out.instr    <= 32'h0000_0013;
+      d_out.pc       <= '0;
+      d_out.pc_plus4 <= '0;
+    end
+    else begin
+      if (~stall) begin
+          d_out.pc       <= d_in.pc;
+          d_out.pc_plus4 <= d_in.pc_plus4;
+      end
 
+      if (flush)
+        d_oout.instr <= 32'h0000_0013;
+      else if (~stall)
+        d_out.instr <= d_in.instr;
+    end
+  end
+  
 endmodule

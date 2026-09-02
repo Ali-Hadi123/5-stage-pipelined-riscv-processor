7f914976b903e188fb03de8e57c0c154f3b9b246
Ali Hadi <77ali.h77@gmail.com>
2026-09-02 17:57:33 -0400

    typo

--- RTL/Pipeline/fd_reg.sv
+++ RTL/Pipeline/fd_reg.sv
@@ -22,10 +22,10 @@ module fd_reg(
       end
 
       if (flush)
-        d_oout.instr <= 32'h0000_0013;
+        d_out.instr <= 32'h0000_0013;
       else if (~stall)
         d_out.instr <= d_in.instr;
     end
   end
-  
+
 endmodule

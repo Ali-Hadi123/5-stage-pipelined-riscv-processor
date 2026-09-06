9dfcfdd61242763471c54b8b74a36db9edd1f504
Ali Hadi <77ali.h77@gmail.com>
2026-09-06 13:10:36 -0400

    extended flush

--- RTL/Hazards/hzrd_unit.sv
+++ RTL/Hazards/hzrd_unit.sv
@@ -13,7 +13,7 @@ module hzrd_unit(
 );
 
     logic lw_stall;
-    logic branch_mispredict;
+    logic branch_mispredict, branch_mispredict_q;
     logic extra_stall_d, extra_stall_q;
 
     always_comb begin
@@ -25,16 +25,20 @@ module hzrd_unit(
         stallF = lw_stall | extra_stall_q;
         stallD = lw_stall | extra_stall_q;
 
-        flushD = branch_mispredict;
-        flushE = lw_stall | branch_mispredict | extra_stall_q;
+        flushD = branch_mispredict | branch_mispredict_q;
+        flushE = lw_stall | branch_mispredict | branch_mispredict_q | extra_stall_q;
         flushM = branch_mispredict;
     end
 
     always_ff @(posedge clk or posedge rst) begin
-        if (rst)
+        if (rst) begin
             extra_stall_q <= 1'b0;
-        else
+            branch_mispredict_q <= 1'b0;
+        end
+        else begin
             extra_stall_q <= extra_stall_d;
+            branch_mispredict_q <= branch_mispredict;
+        end
     end
 
 endmodule

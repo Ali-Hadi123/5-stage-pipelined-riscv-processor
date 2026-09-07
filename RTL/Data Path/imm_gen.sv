8bbf16f8daa96c6886ebc672a33c89c0ecee6c86
Ali Hadi <77ali.h77@gmail.com>
2026-09-06 23:01:29 -0400

    imm gen supports rv64i

--- RTL/Data Path/imm_gen.sv
+++ RTL/Data Path/imm_gen.sv
@@ -1,4 +1,4 @@
-//imm_gen is hardcoded for RV32I since different instruction sets (e.g. RV64I) would require different splicing.
+//imm_gen supports RV32I and RV64I (XLEN = 32 or 64).
 import riscv_pkg::*;
 
 module imm_gen (
@@ -11,11 +11,11 @@ module imm_gen (
   
   always_comb            
     unique case(imm_src)  
-      FMT_I: imm_out = {{20{instr[31]}}, instr[31:20]};
-      FMT_S: imm_out = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-      FMT_B: imm_out = {{19{instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
-      FMT_U: imm_out = {instr[31:12], 12'b0};
-      FMT_J: imm_out = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
-      default: imm_out = {32{1'b0}};
+      FMT_I: imm_out = {{(XLEN-12){instr[31]}}, instr[31:20]};
+      FMT_S: imm_out = {{(XLEN-12){instr[31]}}, instr[31:25], instr[11:7]};
+      FMT_B: imm_out = {{(XLEN-12){instr[31]}}, instr[31], instr[7], instr[30:25], instr[11:8], 1'b0};
+      FMT_U: imm_out = {{(XLEN-32){instr[31]}}, instr[31:12], 12'b0};
+      FMT_J: imm_out = {{(XLEN-20){instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+      default: imm_out = {XLEN{1'b0}};
     endcase
 endmodule
